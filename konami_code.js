c999e87707dc3c39814861603eefcdb2ee3839d1
Brad Schrag <brad.schrag@gmail.com>
2018-07-11 21:36:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,30 @@ const codes = [
 
 function init() {
   // your code here
+  
+  //create variable for documnet.body object
+  
+  const body = document.getElementById('main')
+  
+  //keep track of progess
+  let index = 0
+  
+  //setup event ad
+  body.addEventListener('keydown', function onKeyPressHandler(e) {
+    const key = e.key
+    
+    alert(key)
+    
+    if (key === codes[index]) {
+      index++
+      
+      if (index === codes.length-1) {
+        alert('Congrats!')
+        index = 0
+      }
+    }
+    else {
+      index = 0
+    }
+  })
 }
